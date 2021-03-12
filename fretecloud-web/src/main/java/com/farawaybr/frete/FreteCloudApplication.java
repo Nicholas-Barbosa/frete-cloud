3b1ce22f1a95ddf6c2564c2d81f675b9009a79e9
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-03-11 21:50:57 -0300

    Project moduels

---
+++ fretecloud-web/src/main/java/com/farawaybr/frete/FreteCloudApplication.java
@@ -0,0 +1,14 @@
+package com.farawaybr.frete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreteCloudApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(FreteCloudApplication.class, args);
+	}
+
+}
